5d2ee02e67846920b826170ebdb899602fb51be7
AlexMckey <mckey2002@yandex.ru>
2020-12-03 09:05:20 +0400

    Change WorkPC home dir for puzzle input

--- AoC_Kotlin/src/main/kotlin/InputLib/InputDir.kt
+++ AoC_Kotlin/src/main/kotlin/InputLib/InputDir.kt
@@ -2,8 +2,10 @@ package InputLib
 
 import java.io.File
 import kotlin.sequences.*
-
-val InputDir = """d:\YandexDisk\DevsExercises\AdventOfCode\2020\PuzzleInput\"""
+// Home WorkDir
+//val InputDir = """d:\YandexDisk\DevsExercises\AdventOfCode\2020\PuzzleInput\"""
+// Work WorkDir
+val InputDir = """d:\DevsExercises\AdventOfCode\2020\PuzzleInput\"""
 
 object DayInput {
     fun inputIntsArray(filename: String) = File(InputDir+filename)
