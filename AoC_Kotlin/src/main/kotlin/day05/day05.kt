e5e8e8ad532eb6972312f6a67c6fa49eb569290f
Alex Mc'key <mckey2002@yandex.ru>
2020-12-05 20:43:14 +0400

    Optimization day05 Kotlin solution - both solutions require only one pass through the input sequence (.maxOf and .fold)

--- AoC_Kotlin/src/main/kotlin/day05/day05.kt
+++ AoC_Kotlin/src/main/kotlin/day05/day05.kt
@@ -1,6 +1,8 @@
 package day05
 
 import InputLib.DayInput
+import kotlin.math.max
+import kotlin.math.min
 
 //fun calcSeatID(s: String): Int {
 //    fun posToKoef(ch: Char): Int = if (ch == 'B' || ch == 'R') 1 else -1
@@ -10,7 +12,7 @@ import InputLib.DayInput
 //}
 fun calcSeatID(s: String): Int =
     s.reversed()
-        .fold(1 to 0){ (pow2, acc), c -> pow2 * 2 to acc + (if (c == 'B') 1 else 0) * pow2 }
+        .fold(1 to 0){ (pow2, acc), c -> pow2 * 2 to acc + (if (c == 'B' || c =='R') 1 else 0) * pow2 }
         .second
 
 fun main() {
@@ -21,8 +23,15 @@ fun main() {
     val seatData = rawData.sorted()
     val res1 = calcSeatID(seatData.first())
     println(res1) // 922
+    val res1Opt = DayInput.inputStr("input05.txt").split('\n').dropLast(1)
+        .maxOfOrNull { calcSeatID(it) }
+    println(res1Opt) // 922
     val seatIDs = seatData.map {calcSeatID(it)}
     val holeIdx = seatIDs.zipWithNext(Int::minus).indexOf(2)
     val res2 = seatIDs.first()-holeIdx-1
     println(res2) // 747
+    val res2Opt = rawData.fold(Triple(Int.MAX_VALUE,Int.MIN_VALUE,0)){ (min, max, sum), s ->
+        val seatID = calcSeatID(s)
+        Triple(min(min,seatID), max(max,seatID), sum + seatID) }
+    println((res2Opt.second*(res2Opt.second + 1) - res2Opt.first*(res2Opt.first - 1))/2 - res2Opt.third) // 747
 }
