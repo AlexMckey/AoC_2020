3a90b7536ed630d8ee6a39cd17a9be4ae7878191
Alex Mc'key <mckey2002@yandex.ru>
2020-12-05 20:00:43 +0400

    Optimization day05 Kotlin solution - convert seat String to binary and then convert to Int

--- AoC_Kotlin/src/main/kotlin/day05/day05.kt
+++ AoC_Kotlin/src/main/kotlin/day05/day05.kt
@@ -2,12 +2,16 @@ package day05
 
 import InputLib.DayInput
 
-fun calcSeatID(s: String): Int {
-    fun posToKoef(ch: Char): Int = if (ch == 'B' || ch == 'R') 1 else -1
-    fun calcPos(l: String, seed: Int): Int  =
-        l.fold(seed to seed) {(acc,seed), i -> acc + seed / 2 * posToKoef(i) to seed / 2}.first - 1
-    return 8 * calcPos(s.take(7),128) / 2 + calcPos(s.drop(7),8) / 2
-}
+//fun calcSeatID(s: String): Int {
+//    fun posToKoef(ch: Char): Int = if (ch == 'B' || ch == 'R') 1 else -1
+//    fun calcPos(l: String, seed: Int): Int  =
+//        l.fold(seed to seed) {(acc,seed), i -> acc + seed / 2 * posToKoef(i) to seed / 2}.first - 1
+//    return 8 * calcPos(s.take(7),128) / 2 + calcPos(s.drop(7),8) / 2
+//}
+fun calcSeatID(s: String): Int =
+    s.reversed()
+        .fold(1 to 0){ (pow2, acc), c -> pow2 * 2 to acc + (if (c == 'B') 1 else 0) * pow2 }
+        .second
 
 fun main() {
     val rawData = DayInput.inputStr("input05.txt")
--- AoC_Kotlin/src/main/kotlin/day05/day05p2.ws.kts
+++ AoC_Kotlin/src/main/kotlin/day05/day05p2.ws.kts
@@ -4,3 +4,28 @@ idx
 i.first()
 val seatID = i.first()-idx-1
 seatID
+//val s = "FBFBBFFRLR"
+val s = "BBFFBBFRLL"
+fun posToKoef(ch: Char): Int = when (ch) {
+    'B', 'R' -> 1
+    else -> -1
+}
+fun calcSeatID(s: String): Int {
+    fun calcPos(l: String, seed: Int): Int  =
+        l.fold(seed to seed) {(acc,seed), i -> acc + seed / 2 * posToKoef(i) to seed / 2}.first-1
+    return calcPos(s,1024) / 2
+}
+64*8
+calcSeatID(s)
+val bs = s.replace('B','1')
+    .replace('R','1')
+    .replace('F','0')
+    .replace('L','0')
+bs
+fun chToOne(ch: Char): Int = if (ch in listOf('B','R')) 1 else 0
+val twoPowSeq = generateSequence(1){it*2}
+val pows = twoPowSeq.take(s.length)
+val ps = pows.toList().reversed()
+bs.map { it - '0' }.zip(ps).map { (a,b) -> a*b }.reduce(Int::plus)
+s.reversed().map { chToOne(it) }
+s.reversed().fold(1 to 0){ (pow2, acc), c -> pow2 * 2 to acc + chToOne(c) * pow2 }.second
