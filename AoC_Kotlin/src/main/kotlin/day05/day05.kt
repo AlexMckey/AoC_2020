4994d54f11d2f259e36e742007f88200df350d47
Alex Mc'key <mckey2002@yandex.ru>
2020-12-05 17:25:15 +0400

    Add day05 Kotlin solution - complete

---
+++ AoC_Kotlin/src/main/kotlin/day05/day05.kt
@@ -0,0 +1,24 @@
+package day05
+
+import InputLib.DayInput
+
+fun calcSeatID(s: String): Int {
+    fun posToKoef(ch: Char): Int = if (ch == 'B' || ch == 'R') 1 else -1
+    fun calcPos(l: String, seed: Int): Int  =
+        l.fold(seed to seed) {(acc,seed), i -> acc + seed / 2 * posToKoef(i) to seed / 2}.first - 1
+    return 8 * calcPos(s.take(7),128) / 2 + calcPos(s.drop(7),8) / 2
+}
+
+fun main() {
+    val rawData = DayInput.inputStr("input05.txt")
+        .replace("L","F")
+        .replace("R","B")
+        .split("\n").dropLast(1)
+    val seatData = rawData.sorted()
+    val res1 = calcSeatID(seatData.first())
+    println(res1) // 922
+    val seatIDs = seatData.map {calcSeatID(it)}
+    val holeIdx = seatIDs.zipWithNext(Int::minus).indexOf(2)
+    val res2 = seatIDs.first()-holeIdx-1
+    println(res2) // 747
+}
