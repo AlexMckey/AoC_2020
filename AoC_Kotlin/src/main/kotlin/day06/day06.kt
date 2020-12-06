300ac34a91ba9ae8919a75d2bedfb82583032591
Alex Mc'key <mckey2002@yandex.ru>
2020-12-06 22:41:21 +0400

    Day06 - Some minor improvements (.sumBy and so one)

--- AoC_Kotlin/src/main/kotlin/day06/day06.kt
+++ AoC_Kotlin/src/main/kotlin/day06/day06.kt
@@ -6,18 +6,18 @@ import AoCLib.SomeDay
 object Day06: SomeDay(2020,6) {
     override fun first(data: String): Any? {
         return data.splitByBlankLines()
-            .map {it.replace("\n", "")
+            .sumBy {it.replace("\n", "")
                 .toSet().size
-        }.sum()
+        }
     } // 6930 Time: 35ms
 
     override fun second(data: String): Any? {
         return data.splitByBlankLines()
-            .map { it.split('\n')
-                .map { it.toSet() }
-                .reduce { acc, set -> acc.intersect(set) }
-                .count()
-        }.sum()
+            .sumBy { it.split('\n')
+                .map(String::toSet)
+                .reduce(Set<Char>::intersect)
+                .size
+        }
     } // 3585 Time: 9ms
 }
 
