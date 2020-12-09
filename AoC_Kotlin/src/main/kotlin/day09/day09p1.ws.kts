0487a2e5c50540bf059135a6734068c89bbf9c3f
Alex Mc'key <mckey2002@yandex.ru>
2020-12-09 14:11:01 +0400

    Add day09 Kotlin solution - complete

---
+++ AoC_Kotlin/src/main/kotlin/day09/day09p1.ws.kts
@@ -0,0 +1,32 @@
+import AoCLib.pairs
+import AoCLib.sum
+
+val l = listOf(35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576)
+val w5_1 = l.take(5)
+val w5_2 = l.drop(1).take(5)
+val w5_3 = l.drop(2).take(5)
+w5_1
+w5_2
+w5_3
+val w5pairs1 = w5_1.pairs().map{it.first + it.second}.toList()
+val w5pairs2 = w5_2.pairs().map{it.first + it.second}.toList()
+val w5pairs3 = w5_3.pairs().map{it.first + it.second}.toList()
+w5pairs1
+w5pairs2
+w5pairs3
+w5pairs1.contains(l[5])
+w5pairs2.contains(l[6])
+w5pairs3.contains(l[7])
+l.windowed(6).map {
+    it.last() to it.take(5).pairs().map{it.first + it.second}.contains(it.last())
+}
+l.windowed(6).find {
+    val num = it.last()
+    val nums = it.take(5)
+    !nums.pairs().map { it.sum() }.contains(num)
+}?.last()
+fun windowsSeq(windowCount: Int) = generateSequence(Triple(0,0,true)) {
+    val w = l.subList(it.first,it.first+windowCount+1)
+    Triple(it.first + 1,w.last(),w.take(windowCount).pairs().map{it.first + it.second}.contains(w.last()))
+}
+windowsSeq(5).dropWhile { it.third }.first().second
---
+++ AoC_Kotlin/src/main/kotlin/day09/day09p2.ws.kts
@@ -0,0 +1,37 @@
+import AoCLib.pairs
+import AoCLib.series
+
+val l = listOf(35,20,15,25,47,40,62,55,65,95,102,117,150,182,127,219,299,277,309,576)
+    .map { it.toLong() }
+fun windowsSeq(windowCount: Int, seq: List<Long>) = generateSequence(Triple(0,0L,true)) {
+    val w = seq.subList(it.first,it.first+windowCount+1)
+    Triple(it.first + 1,w.last(),w.take(windowCount).pairs().map{it.first + it.second}.contains(w.last()))
+}
+val res = windowsSeq(5,l).dropWhile { it.third }.first()
+val resNum = res.second
+resNum
+val w = l.take(res.first+5-1)
+fun checkWindow(winNums: List<Long>, winCnt: Int, num: Long) =
+    winNums.asSequence()
+        .windowed(winCnt)
+        .map { it.sum() to it }
+        .find { it.first == num }
+fun findSeqs4Num(winNums: List<Long>, maxWindows: Int, num: Long): List<Long>? {
+    var i = 2
+    var res: Pair<Long,List<Long>>? = null
+    do {
+        res = checkWindow(winNums,i,num)
+        i++
+    } while (res == null && i < maxWindows)
+    return res?.second
+}
+val w2 = w.asSequence().windowed(2).map { it.sum() }.filter { it <= resNum }
+w2.toList()
+checkWindow(w,2,resNum)
+val w3 = w.asSequence().windowed(3).map { it.sum() }.filter { it <= resNum }
+w3.toList()
+checkWindow(w,3,resNum)
+val w4 = w.asSequence().windowed(4).map { it.sum() }.filter { it <= resNum }
+w4.toList()
+checkWindow(w,4,resNum)
+findSeqs4Num(w,5,resNum)
