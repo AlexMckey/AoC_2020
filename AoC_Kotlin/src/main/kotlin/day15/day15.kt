b10f860e783414d885545f7e0edbe043bc3732f0
Alex Mc'key <mckey2002@yandex.ru>
2020-12-15 23:15:12 +0400

    Add day15 kotlin solution - some realization (by Map and by List)

--- AoC_Kotlin/src/main/kotlin/day15/day15.kt
+++ AoC_Kotlin/src/main/kotlin/day15/day15.kt
@@ -3,30 +3,40 @@ package day15
 import AoCLib.SomeDay
 
 object Day15:SomeDay(2020,15) {
-    private tailrec fun elvesGame(nums: MutableMap<Int,Int>, num: Int, i: Int, endCnt: Int): Int {
-        if (i >= endCnt) return num
-        val t = if (!nums.containsKey(num)) 0
-        else i - nums[num]!!
-        nums[num] = i
-        return elvesGame(nums, t, i + 1, endCnt)
+    fun elvesGameList(s: String, endCnt:Int): Int {
+        val ns = s.split(',').map { it.toInt() }.toMutableList()
+        val num = ns.removeAt(ns.lastIndex)
+        tailrec fun recHelper(num: Int, i: Int): Int {
+            if (i >= endCnt) return num
+            val idx = ns.lastIndexOf(num)
+            ns.add(num)
+            return recHelper(if (idx >= 0) i - idx - 1 else 0, i + 1)
+        }
+        return recHelper(num, ns.size+1)
     }
 
-    override fun first(data: String): Any? {
-        val ns = data.split(',')
+    override fun first(data: String): Any? =
+        elvesGameList(data,2020)
+    // 421 Time: 10ms
+
+    fun elvesGameMap(s: String, endCnt: Int): Int {
+        val ns = s.split(',')
+            .mapIndexed { index, i ->  i.toInt() to index + 1 }
+        val (num, idx) = ns.last()
         val nums = ns.dropLast(1)
-            .mapIndexed { index, s ->  s.toInt() to index+1 }
             .toMap().toMutableMap()
-        return elvesGame(nums,ns.last().toInt(),nums.size+1,2020)
+        tailrec fun recHelper(num: Int, i: Int): Int {
+            if (i >= endCnt) return num
+            val t = i - nums.getOrDefault(num,i)
+            nums[num] = i
+            return recHelper(t, i + 1)
+        }
+        return recHelper(num, idx)
     }
 
-    override fun second(data: String): Any? {
-        //val s = "3,1,2"
-        val ns = data.split(',')
-        val nums = ns.dropLast(1)
-            .mapIndexed { index, s ->  s.toInt() to index+1 }
-            .toMap().toMutableMap()
-        return elvesGame(nums,ns.last().toInt(),nums.size+1,30000000)
-    }
+    override fun second(data: String): Any? =
+        elvesGameMap(data,30000000)
+    // 436 Time: 4041ms
 }
 
 fun main() = SomeDay.mainify(Day15)
