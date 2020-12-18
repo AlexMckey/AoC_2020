b13af69e255b6bccad17e124e136e6af4867cb06
Alex Mc'key <mckey2002@yandex.ru>
2020-12-18 16:15:41 +0400

    In day17 Kotlin solution some improve and optimization

--- AoC_Kotlin/src/main/kotlin/day17/day17.kt
+++ AoC_Kotlin/src/main/kotlin/day17/day17.kt
@@ -6,38 +6,30 @@ import AoCLib.Point4D
 import AoCLib.SomeDay
 
 object Day17:SomeDay(2020,17) {
-    private fun <T: Nears<T>>next(cube: Set<T>): Set<T> =
-        cube.filter { it.near()
-            .intersect(cube)
-            .size in 2..3 }
-            .union(cube
-                .flatMap { it.near() }
-                .groupingBy { it }.eachCount()
-                .filter { it.value == 3 }
-                //.groupBy { it }
-                //.filter { it.value.size == 3 }
-                .map { it.key })
-    override fun first(data: String): Any? {
-        var cube = data.lines()
+    private fun <T: Nears<T>> next(cube: Set<T>): Set<T> =
+        cube.flatMap { it.near() }
+            .groupingBy { it }.eachCount()
+            .filter { it.value == 3 || it.value == 2 && cube.contains(it.key) }
+            .keys
+    private fun <T: Nears<T>> toPocketDimensions(s: String, f: (x: Int, y: Int, z: Int, w: Int) -> T): Set<T> =
+        s.lines()
             .flatMapIndexed{ y, l -> l
-                .mapIndexed { x, ch ->
-                    if (ch == '#') Point3D(x,y,0)
-                    else null
-                } }.filterNotNull().toSet()
+            .mapIndexed { x, ch ->
+            if (ch == '#') f(x,y,0,0)
+            else null
+        } }.filterNotNull().toSet()
+
+    override fun first(data: String): Any? {
+        var cube = toPocketDimensions(data) { x,y,z,_ -> Point3D(x,y,z)}
         repeat(6) {cube = next(cube) }
         return cube.size
-    } // 317 Time: 52ms
+    } // 317 Time: 39ms
 
     override fun second(data: String): Any? {
-        var hyperCube = data.lines()
-            .flatMapIndexed{ y, l -> l
-                .mapIndexed { x, ch ->
-                    if (ch == '#') Point4D(x,y,0,0)
-                    else null
-                } }.filterNotNull().toSet()
+        var hyperCube = toPocketDimensions(data) { x,y,z,w -> Point4D(x,y,z,w)}
         repeat(6) {hyperCube = next(hyperCube) }
         return hyperCube.size
-    } // 1692 Time: 183ms
+    } // 1692 Time: 94ms
 }
 
 fun main() = SomeDay.mainify(Day17)
